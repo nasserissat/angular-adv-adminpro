f8c3adb3cda4cb955223a390d4e63bcf76dc3885
Nasser <nasseremil25@gmail.com>
2022-09-13 21:03:03 -0400

    fin de la seccion 4. Reorganice las rutas y los modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+// Modules
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Components
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+  ],
+  exports: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
